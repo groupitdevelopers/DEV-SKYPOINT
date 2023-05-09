31824d5abd87f66081d6cab10003f919b47cf4e8
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-10 08:15:15 +1000

    lazy load component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,14 @@
-import * as React from "react";
-import Header from "../components/header"
-import Footer from "../components/footer"
+import React, { lazy } from 'react';
+// import Header from "../components/header"
+// import Footer from "../components/footer"
 
 const Layout = ({ location, title, children }) => {
+  
+  const Header = lazy(() => import('../components/header'))
+  const Footer = lazy(() => import('../components/footer'))
   const rootPath = `${__PATH_PREFIX__}/`;
   const isRootPath = location.pathname === rootPath;
+
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
