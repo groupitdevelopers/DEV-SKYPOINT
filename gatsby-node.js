76f16aef2d9208514f317c13da902b88766d8b7e
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-16 10:58:53 +1000

    preview test

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,52 +6,94 @@ exports.createPages = async ({ graphql, actions }) => {
   const homePage = path.resolve(`./src/templates/page.jsx`)
   const subPage = path.resolve(`./src/templates/subPage.jsx`)
 
-  const queryResult = await graphql(`
-    query {
-      allPrismicSubpage {
-        nodes {
-          id
-          url
-        }
-      }
-    }
-  `)
-
-  const redirections = await graphql(`
-    query {
-      prismicRedirections {
-        data {
-          redirections {
-            from
-            to
-          }
-        }
-      }
-    }
-  `)
+  // const pages = await graphql(`
+  //   query {
+  //     allPrismicSubpage {
+  //       nodes {
+  //         id
+  //         url
+  //         type
+  //       }
+  //     }
+  //     allPrismicHomepage {
+  //       nodes {
+  //         id
+  //         url
+  //         type
+  //       }
+  //     }
+  //   }
+  // `)
 
   createPage({
     path: "/",
     component: homePage,
     context: {
-      id: "homePage"
-    },
-  });
+       id: "homePage"
+     },
+   });
 
-  const redirects = redirections.data.prismicRedirections.data.redirections
+  // for (const page of queryResult.data.allPrismicSubpage.nodes ?? []) {
+  //     createPage({
+  //       path: page.url,
+  //       component: path.resolve(__dirname, 'src/templates/subPage.js'),
+  //       context: { id: page.id }
+  //     })
+  //   }
 
-  redirects.forEach(redirect => {
-    createRedirect({
-      fromPath: redirect.from,
-      toPath: redirect.to
-    })
-  })
+  // pages.data.allPrismicSubpage.nodes.forEach((page) => {
+  //   createPage({
+  //     path: page.url,
+  //     component: path.resolve(__dirname, 'src/templates/subPage.js'),
+  //     context: { ...page },
+  //   })
+  // })
 
-  for (const page of queryResult.data.allPrismicSubpage.nodes ?? []) {
-    createPage({
-      path: page.url,
-      component: subPage,
-      context: { id: page.id }
-    })
-  }
+  // pages.data.allPrismicHomepage.nodes.forEach((page) => {
+  //   createPage({
+  //     path: "/",
+  //     component: path.resolve(__dirname, 'src/templates/page.js'),
+  //     context: { ...page },
+  //   })
+  // })
+
+  // const redirections = await graphql(`
+  //   query {
+  //     prismicRedirections {
+  //       data {
+  //         redirections {
+  //           from
+  //           to
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+  // const redirects = redirections.data.prismicRedirections.data.redirections
+
+  // redirects.forEach(redirect => {
+  //   createRedirect({
+  //     fromPath: redirect.from,
+  //     toPath: redirect.to
+  //   })
+  // })
+
+
+  // createPage({
+  //   path: "/",
+  //   component: homePage,
+  //   context: {
+  //     id: "homePage"
+  //   },
+  // });
+
+
+  // for (const page of queryResult.data.allPrismicSubpage.nodes ?? []) {
+  //   createPage({
+  //     path: page.url,
+  //     component: subPage,
+  //     context: { id: page.id }
+  //   })
+  // }
 }
